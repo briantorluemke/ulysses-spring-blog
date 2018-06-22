779bb6eb03039d8f55a7885da5340703e442013b
Brian Torluemke <briantorluemke@gmail.com>
2018-06-22 13:58:09 -0500

    created a seeder for easy posts for placeholders

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -69,4 +69,15 @@ public class PostController {
         postService.delete(id);
         return "redirect:/posts";
     }
+
+    @GetMapping("/seed")
+    public String seedPosts() {
+        postService.save(new Post("Choose the perfect design", "Create a beautiful blog that fits your style. Choose from a selection of easy-to-use templates – all with flexible layouts and hundreds of background images – or design something new."));
+        postService.save(new Post("Get a free domain", "Give your blog the perfect home. Get a free blogspot.com domain or buy a custom domain with just a few clicks."));
+        postService.save(new Post("Earn money", "Get paid for your hard work. Google AdSense can automatically display relevant targeted ads on your blog so that you can earn income by posting about your passion."));
+        postService.save(new Post("Know your audience", "Find out which posts are a hit with Blogger’s built-in analytics. You’ll see where your audience is coming from and what they’re interested in. You can even connect your blog directly to Google Analytics for a more detailed look."));
+        postService.save(new Post("Hang onto your memories", "Save the moments that matter. Blogger lets you safely store thousands of posts, photos, and more with Google for free."));
+        postService.save(new Post("Join millions of others", "Whether sharing your expertise, breaking news, or whatever’s on your mind, you’re in good company on Blogger. Sign up to discover why millions of people have published their passions here."));
+        return "redirect:/posts";
+    }
 }
